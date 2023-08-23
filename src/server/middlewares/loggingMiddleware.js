be8fa72b5f3b69ccdb5f7fe3c74d4454cca16593
andrei <Your Email>
2023-08-23 20:29:59 +0200

    feat (logger) Middleware logger

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -3,7 +3,7 @@ const loggingMiddleware = (db) =>
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
-        // Persist this info on DB
+        db.logging.create(  {ip, headers, originalUrl}  )
         next();
     }
 
