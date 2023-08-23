730a64e491861f86a62567d62a448125dacee1fb
andrei <Your Email>
2023-08-23 22:04:17 +0200

    feat (logger) added log functionality

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,10 +1,12 @@
 const loggingMiddleware = (db) =>
     (req, res, next) => {
-        const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        const headers = JSON.stringify(req.headers);
-        const originalUrl = req.originalUrl;
-        db.logging.create(  {ip, headers, originalUrl}  )
-        next();
+            console.info('Received new request. Logging into database...')
+            db.logging.create({
+                    ip: req.headers['host'].split(':')[0].trim(),
+                    header: JSON.stringify(req.headers),
+                    action: req.originalUrl
+            })
+            next()
     }
 
 module.exports = loggingMiddleware;
