50251253aaf6f7cf9fc846226fa8d0904c2767f1
andrei <Your Email>
2023-08-23 20:44:01 +0200

    feat (getPeople) get person by id + formatter upgrade

---
+++ src/server/endpoints/hfswapi/get_people.js
@@ -0,0 +1,19 @@
+const {_isWookieeFormat} = require("../../service/format");
+const {peopleFactory} = require("../../../app/People");
+const getPeopleEndpoint = async (req, res, app) => {
+    const {id} = req.params
+
+    const people = await peopleFactory(id, _isWookieeFormat(req) ? "wookiee" : "")
+    if (!people.name) {
+        return res.status(400).json({message: "Given person does not exists by id: " + id})
+    }
+
+    return res.status(200).json({
+        name: people.getName(),
+        mass: people.getMass(),
+        height: people.getHeight(),
+        homeworldName: people.getHomeworldName(),
+        homeworldId: people.getHomeworlId(),
+    })
+}
+module.exports = {getPeopleEndpoint}
---
+++ src/server/service/format.js
@@ -0,0 +1,6 @@
+const _isWookieeFormat = ( req ) => {
+
+    if(req.query.format && req.query.format == 'wookiee')return true
+    return false
+}
+module.exports = { _isWookieeFormat}
