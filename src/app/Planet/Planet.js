a4306791bdfe92d3ad47fdf9667cc3770ebbd466
andrei <Your Email>
2023-08-23 22:43:23 +0200

    feat (planet) planet entity done

--- src/app/Planet/Planet.js
+++ src/app/Planet/Planet.js
@@ -1,17 +1,43 @@
+const {db, swapiFunctions} = require('../index')
+
 class Planet {
-    constructor(id){
-        throw new Error('To be implemented');
+
+    constructor(id) {
+        this.id = id
+        this.name = null
+        this.gravity = null
+        this.endpoint = 'https://swapi.dev/api/planets/{id}'
     }
 
-    async init(){
-        throw new Error('To be implemented');
+    async init() {
+        const db_planet = await db.swPlanet.findByPk(this.id, {attributes: ['name', 'gravity']})
+        if (db_planet) {
+            this.name = db_planet.name
+            this.gravity = db_planet.gravity
+        } else {
+            await this.retrieveFromApi();
+        }
+    }
+
+    async retrieveFromApi() {
+        const url = this.endpoint.replace("{id}", this.id)
+        const body = await swapiFunctions.genericRequest(url, 'GET', null)
+
+        this.name = body.name
+        this.gravity = parseFloat(body.gravity.replace(/[^\d.-]/g, ''))
+
+        await db.swPlanet.create({id: this.id, name: this.name, gravity: this.gravity})
     }
 
     getName() {
-        return this.name;
+        return this.name
     }
 
     getGravity() {
-        return this.gravity;
+        return this.gravity
     }
+
+
 }
+
+module.exports = Planet
